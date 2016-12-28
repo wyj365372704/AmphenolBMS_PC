e949751ff997bc7852c685398ebe05117212efb6
ningfy <43516784@qq.com>
2016-12-28 11:51:43 +0800

    销售出货相关表

---
+++ src/com/eclink/hgpj/resource/vo/ZPLDTLVO.java
@@ -0,0 +1,221 @@
+package com.eclink.hgpj.resource.vo;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.eclink.hgpj.base.BaseVO;
+
+/**
+ * @Title: 资源值对象类  
+ * @Description: 
+
+ * @version 1.0
+ * 
+ */
+public class ZPLDTLVO extends BaseVO{
+	
+	private static final long serialVersionUID = 2317968819354022841L;
+	
+	private String pldno;
+	
+	private BigDecimal pldln;
+	
+	private String c6aenb;
+	
+	private String c6dccd;
+	
+	private String c6cvnb;
+	
+	private String ponum;
+	
+	private String house;
+	
+	private BigDecimal cdfcnb;
+
+	private BigDecimal addrnb;
+
+	private BigDecimal adaasz;
+	
+	private String itnbr;
+	
+	private String unmsr;
+	
+	private String plsub;
+	
+	private String plloc;
+	
+	private BigDecimal plqty;
+	
+	private String fpost;
+	
+	private BigDecimal wght1;
+	
+	private String wtum1;
+		
+	private BigDecimal wght2;
+	
+	private String wtum2;
+
+	public String getPldno() {
+		return pldno;
+	}
+
+	public void setPldno(String pldno) {
+		this.pldno = pldno;
+	}
+
+	public BigDecimal getPldln() {
+		return pldln;
+	}
+
+	public void setPldln(BigDecimal pldln) {
+		this.pldln = pldln;
+	}
+
+	public String getC6aenb() {
+		return c6aenb;
+	}
+
+	public void setC6aenb(String c6aenb) {
+		this.c6aenb = c6aenb;
+	}
+
+	public String getC6dccd() {
+		return c6dccd;
+	}
+
+	public void setC6dccd(String c6dccd) {
+		this.c6dccd = c6dccd;
+	}
+
+	public String getC6cvnb() {
+		return c6cvnb;
+	}
+
+	public void setC6cvnb(String c6cvnb) {
+		this.c6cvnb = c6cvnb;
+	}
+
+	public String getPonum() {
+		return ponum;
+	}
+
+	public void setPonum(String ponum) {
+		this.ponum = ponum;
+	}
+
+	public String getHouse() {
+		return house;
+	}
+
+	public void setHouse(String house) {
+		this.house = house;
+	}
+
+	public BigDecimal getCdfcnb() {
+		return cdfcnb;
+	}
+
+	public void setCdfcnb(BigDecimal cdfcnb) {
+		this.cdfcnb = cdfcnb;
+	}
+
+	public BigDecimal getAddrnb() {
+		return addrnb;
+	}
+
+	public void setAddrnb(BigDecimal addrnb) {
+		this.addrnb = addrnb;
+	}
+
+	public BigDecimal getAdaasz() {
+		return adaasz;
+	}
+
+	public void setAdaasz(BigDecimal adaasz) {
+		this.adaasz = adaasz;
+	}
+
+	public String getItnbr() {
+		return itnbr;
+	}
+
+	public void setItnbr(String itnbr) {
+		this.itnbr = itnbr;
+	}
+
+	public String getUnmsr() {
+		return unmsr;
+	}
+
+	public void setUnmsr(String unmsr) {
+		this.unmsr = unmsr;
+	}
+
+	public String getPlsub() {
+		return plsub;
+	}
+
+	public void setPlsub(String plsub) {
+		this.plsub = plsub;
+	}
+
+	public String getPlloc() {
+		return plloc;
+	}
+
+	public void setPlloc(String plloc) {
+		this.plloc = plloc;
+	}
+
+	public BigDecimal getPlqty() {
+		return plqty;
+	}
+
+	public void setPlqty(BigDecimal plqty) {
+		this.plqty = plqty;
+	}
+
+	public String getFpost() {
+		return fpost;
+	}
+
+	public void setFpost(String fpost) {
+		this.fpost = fpost;
+	}
+
+	public BigDecimal getWght1() {
+		return wght1;
+	}
+
+	public void setWght1(BigDecimal wght1) {
+		this.wght1 = wght1;
+	}
+
+	public String getWtum1() {
+		return wtum1;
+	}
+
+	public void setWtum1(String wtum1) {
+		this.wtum1 = wtum1;
+	}
+
+	public BigDecimal getWght2() {
+		return wght2;
+	}
+
+	public void setWght2(BigDecimal wght2) {
+		this.wght2 = wght2;
+	}
+
+	public String getWtum2() {
+		return wtum2;
+	}
+
+	public void setWtum2(String wtum2) {
+		this.wtum2 = wtum2;
+	}
+	
+	
+  
+}
